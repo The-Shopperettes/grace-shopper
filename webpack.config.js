58bdaf634a4b634d0f2df5648794cdf5a8a48519
Monique Hayes <moniqueehayes5@gmail.com>
2023-02-28 16:10:05 -0600

    added boostrap link in html & rule in webpack.js

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,10 @@ module.exports = {
           presets: ['@babel/preset-env', '@babel/preset-react'],
         },
       },
+      {
+        test: /\.(css)$/,
+        loader: 'css-loader'
+      }
     ],
   },
 };
