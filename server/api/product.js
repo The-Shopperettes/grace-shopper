8163a76323c6216e2df5e1e0c9d254d82c5053bc
judazzle <scujkuo@gmail.com>
2023-02-28 14:29:46 -0600

    included pagination for get all products route

--- server/api/product.js
+++ server/api/product.js
@@ -4,13 +4,31 @@ const {Product, User, Cart} = require('../db');
 // get all products
 router.get('/', async (req, res, next) => {
     try{
-        res.json(await Product.findAll());
+        // set page to the query, or default to 1
+        const page = req.query.page || 1;
+        // set the perPage limit to the query, or default to 20
+        const perPage = req.query.perPage || 20;
+
+        // calculate the offset
+        const offset = (page - 1) * perPage;
+
+        // get all the products
+        res.json(await Product.findAll({
+            offset,
+            limit: perPage,
+        }));
     } catch (err) {
         next(err);
     }
 });
 
-// TODO: single product
-
+// get single product
+router.get('/:productId', async (req, res, next) => {
+    try{
+        res.json(await Product.findByPk(req.params.productId));
+    } catch(err) {
+        next(err);
+    }
+})
 
 module.exports = router;
