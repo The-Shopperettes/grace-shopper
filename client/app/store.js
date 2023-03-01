93b42c882d5a8226639c72ce2963882b06ce29c1
judazzle <scujkuo@gmail.com>
2023-03-01 15:22:26 -0600

    /client/app/store.js: added products and singleProduct reducer

--- client/app/store.js
+++ client/app/store.js
@@ -3,12 +3,16 @@ import logger from 'redux-logger';
 import authReducer from '../features/auth/authSlice';
 import cartReducer from '../features/cart/cartSlice';
 import userReducer from '../features/user/userSlice';
+import productsReducer from '../features/products/productsSlice';
+import singleProductReducer from '../features/products/singleProductSlice';
 
 const store = configureStore({
   reducer: { 
     auth: authReducer,
     cart: cartReducer,
-    user: userReducer
+    user: userReducer,
+    products: productsReducer,
+    singleProduct: singleProductReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
