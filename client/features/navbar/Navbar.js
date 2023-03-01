8f1f3f2b10f86f3f1c8b8d2c7862485c7ebf28fc
Carla Herrera <cgherrera07@gmail.com>
2023-03-01 16:53:34 -0500

    Add cart icon with badge with sample count of items in cart. Add link to cart button to route to cart when clicked

--- client/features/navbar/Navbar.js
+++ client/features/navbar/Navbar.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { Link, useNavigate } from 'react-router-dom';
-import { logout } from '../../app/store';
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { logout } from "../../app/store";
+import { Button, Badge } from "react-bootstrap";
 
 const Navbar = () => {
-  const isLoggedIn = useSelector((state) => !!state.auth.me);
+  const isLoggedIn = useSelector((state) => !!state.auth.me.id);
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const logoutAndRedirectHome = () => {
     dispatch(logout());
-    navigate('/login');
+    navigate("/login");
   };
 
   return (
@@ -29,6 +30,12 @@ const Navbar = () => {
             {/* The navbar will show these links before you log in */}
             <Link to="/login">Login</Link>
             <Link to="/signup">Sign Up</Link>
+            <Link to="/cart">
+              <Button variant="cart">
+                🛒<Badge bg="secondary">10</Badge>
+                <span className="visually-hidden">cart items</span>
+              </Button>
+            </Link>
           </div>
         )}
       </nav>
