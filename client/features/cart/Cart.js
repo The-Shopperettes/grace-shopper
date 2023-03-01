c2b3f72e2ef5a92d069fd21079f4be76feb23947
Leah Treidler <leahtreidler@gmail.com>
2023-03-01 13:12:39 -0500

    Created cart component and cards

--- client/features/cart/Cart.js
+++ client/features/cart/Cart.js
@@ -1,6 +1,7 @@
 import React, {useEffect} from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { selectCart, fetchCart } from './cartSlice';
+import { Card, Button, Container, Stack, Row, Col } from 'react-bootstrap';
 
 
 const Cart = () => {
@@ -12,7 +13,27 @@ const Cart = () => {
     }, [dispatch])
 
     return (
-        <div>Cart</div>
+    <Container>
+        { cartItems ?
+        <Stack> 
+            {
+               cartItems.map(({id, product, qty}) => {
+                return (
+                <Card>
+                    <Card.Body>
+                        <Card.Img src={product.mediumImg}></Card.Img>
+                        <Card.Title>{product.name}</Card.Title>
+                        <Card.Text>
+                        <p>Quantity: {qty}</p>
+                        <p>Price: ${product.price}.00</p>
+                        </Card.Text>
+                    </Card.Body>
+                </Card>)
+            })
+            }
+        </Stack> : null
+        }
+    </Container>
     )
 
 }
