e6aa75d1c2573910911a6e4ea52064762235ca34
Leah Treidler <leahtreidler@gmail.com>
2023-03-07 20:42:37 -0500

    Updates tto orders and add product

--- client/features/user/Orders.js
+++ client/features/user/Orders.js
@@ -1,21 +1,23 @@
 import React from "react";
-import { Accordion, Card, Stack } from "react-bootstrap";
+import { Accordion, Card, Stack, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 const Orders = ({ orders }) => {
   return (
     <Accordion defaultActiveKey="">
-      {orders.map(({ id, cartItems }) => {
+      {orders.map(({ id, cartItems}) => {
         return (
           <Accordion.Item key={id} eventKey={id}>
             <Accordion.Header>Order #{id}</Accordion.Header>
             <Accordion.Body>
+            <Row xs={1} md={3} gap={3}>
               {cartItems.map(({ id, product, qty }) => (
-                <Card key={id} className="mt-3">
+                <Col gap={3} key={id}>
+                <Card key={id} className="mt-3" >
                   <Stack direction="horizontal">
                     <Card.Img
                       src={product.thumbnail}
-                      style={{ height: "10rem", width: "auto" }}
+                      style={{ height: "7rem", width: "auto" }}
                     ></Card.Img>
                     <Card.Body>
                       <Card.Text>Price: ${product.price}.00</Card.Text>
@@ -26,7 +28,9 @@ const Orders = ({ orders }) => {
                     </Card.Body>
                   </Stack>
                 </Card>
+                </Col>
               ))}
+              </Row>
             </Accordion.Body>
             <Accordion.Body>
             <h2>
